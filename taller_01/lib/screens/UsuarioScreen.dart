61f2ec746fa8e469e88296c01f135c0549eb2b17
DetkenChristopher <juangasar@gmail.com>
2025-06-10 20:33:46 -0500

    BottomTab

---
+++ taller_01/lib/screens/UsuarioScreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:taller_01/Navigation/bottomTabNavigation.dart';
+
+class PerfilUser extends StatelessWidget {
+  const PerfilUser({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Pantalla1"),),
+      body: Center(child: Text("Screen 1")),
+      bottomNavigationBar: BottomTab(),
+      
+    );
+  }
+}
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const BottomTab(); 
+  }
+}
