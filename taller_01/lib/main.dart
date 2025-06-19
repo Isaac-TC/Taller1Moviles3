94fcde63ed71b08a0ec8c28bfebf902b7d0a2a29
Isaac-TC <iseduardot92@gmail.com>
2025-06-18 22:22:01 -0500

    subir imagenes

--- taller_01/lib/main.dart
+++ taller_01/lib/main.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-import 'package:taller_01/screens/splash_screen.dart';   // ← NUEVO
+import 'package:taller_01/screens/splash_screen.dart';
 import 'package:taller_01/screens/loginScreen.dart';
 import 'package:taller_01/screens/registerScreen.dart';
 import 'package:taller_01/screens/homeScreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Inicializa Firebase
   await Firebase.initializeApp();
+
+  // Inicializa Supabase
+  await Supabase.initialize(
+    url: 'https://bytkeraqgtcfberoksnn.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5dGtlcmFxZ3RjZmJlcm9rc25uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2ODM2OTMsImV4cCI6MjA2NTI1OTY5M30.hZUJ95AA-0SQfo_de3t9XWAQlyc-hX6Hi_zdLkKEBmI',
+  );
+
   runApp(const MyApp());
 }
 
@@ -18,9 +29,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'App Registro',                // (sin cambios)
+      title: 'App Registro',
       debugShowCheckedModeBanner: false,
-      // mismo tema oscuro pero con rojo como color base
       theme: ThemeData.dark().copyWith(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
         scaffoldBackgroundColor: Colors.black,
@@ -31,11 +41,9 @@ class MyApp extends StatelessWidget {
           style: ElevatedButton.styleFrom(backgroundColor: Colors.redAccent),
         ),
       ),
-
-      // rutas (solo se añade Splash y se mueve Login)
-      initialRoute: '/',                    // ahora muestra Splash primero
+      initialRoute: '/',
       routes: {
-        '/':         (_) => const SplashScreen(), // ← NUEVO
+        '/':         (_) => const SplashScreen(),
         '/login':    (_) => const LoginScreen(),
         '/register': (_) => const RegisterScreen(),
         '/home':     (_) => const HomeScreen(),
